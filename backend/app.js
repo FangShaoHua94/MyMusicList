482b829f8d4789966fd50bb98fa52391815d79c5
FangShaoHua94 <daythehangedman@hotmail.com>
2021-01-30 15:19:30 +0800

    Set up

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello to MY MUSIC LIST api");
+})
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost:27017/MyMusicList', { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((e) => console.log(e.message));
